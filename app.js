241e215ff6dcfce3dfcf407e6e13ee019ea856c6
tlhaeroglu <talha44el@gmail.com>
2022-04-17 02:24:14 +0300

    first

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', (req,res) =>{
+    res.render('index')
+})
+
+app.get('/admin', (req,res) =>{
+    res.render('admin/index')
+})
+
+
+app.listen(3000, () => {
+    console.log('server started');
+})
